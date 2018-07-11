d4abebf0083e34e9da820f54d041cb93ddf1be10
fbulbuc <fbulbuc@ualberta.ca>
2018-07-10 20:30:17 -0700

    Implemented opening handshake.

--- tests/test_handshake.py
+++ tests/test_handshake.py
@@ -14,9 +14,64 @@ class TestHandShake(unittest.TestCase):
     def test_digest(self):
         """Test the calculation of the Accept-Key."""
         client_key = 'dGhlIHNhbXBsZSBub25jZQ=='
-        expected = b's3pPLMBiTxaQ9kYGzzhZRbK+xOo='
+        expected = 's3pPLMBiTxaQ9kYGzzhZRbK+xOo='
+
         result = WS.WebSocketServer._digest(client_key)
         self.assertEqual(expected, result)
+
+    def test_handshake_valid(self):
+        """Test the handshake output for a valid upgrade request."""
+        upgrade_request = (
+            "GET /chat HTTP/1.1\r\n"
+            "Host: example.com:8000\r\n"
+            "Upgrade: websocket\r\n"
+            "Connection: Upgrade\r\n"
+            "Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\n"
+            "Sec-WebSocket-Version: 13\r\n\r\n"
+        )
+
+        expected_upgrade_response = (
+            "HTTP/1.1 101 Switching Protocols\r\n"
+            "Upgrade: websocket\r\n"
+            "Connection: Upgrade\r\n"
+            "Sec-WebSocket-Accept: s3pPLMBiTxaQ9kYGzzhZRbK+xOo=\r\n\r\n"
+        )
+
+        ws = WS.WebSocketServer(None, None)
+
+        valid, upgrade_response = ws._opening_handshake(None, upgrade_request)
+        self.assertTrue(valid)
+        self.assertEqual(expected_upgrade_response, upgrade_response)
+
+    def test_handshake_invalid(self):
+        """Test the handshake output for a valid upgrade request."""
+        upgrade_request_bad_upgrade = (
+            "GET /chat HTTP/1.1\r\n"
+            "Host: example.com:8000\r\n"
+            "Upgrade: http\r\n"
+            "Connection: Upgrade\r\n"
+            "Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\n"
+            "Sec-WebSocket-Version: 13\r\n\r\n"
+        )
+
+        upgrade_request_bad_connection = (
+            "GET /chat HTTP/1.1\r\n"
+            "Host: example.com:8000\r\n"
+            "Upgrade: websocket\r\n"
+            "Connection: Maintain\r\n"
+            "Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\n"
+            "Sec-WebSocket-Version: 13\r\n\r\n"
+        )
+
+        ws = WS.WebSocketServer(None, None)
+
+        valid, _ = ws._opening_handshake(
+            None, upgrade_request_bad_upgrade)
+        self.assertFalse(valid)
+
+        valid, _ = ws._opening_handshake(
+            None, upgrade_request_bad_connection)
+        self.assertFalse(valid)
 
 
 if __name__ == "__main__":
--- websocket/WebSocketServer.py
+++ websocket/WebSocketServer.py
@@ -15,7 +15,7 @@ class WebSocketServer():
     )
 
     def __init__(self, ip, port):
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = None
         self.ip = ip
         self.port = port
         self.clients = {}
@@ -49,23 +49,41 @@ class WebSocketServer():
         for client in self.clients:
             self.send(data, client)
 
-    def _opening_handshake(self, client):
+    def _opening_handshake(self, client, data):
         """Complete opening handshake between the client and the server.
 
         :param client: The client to complete the handshake with.
+        :param data: The raw data included in the upgrade request.
+
+        :returns (valid, response): A tuple containing a boolean flag 
+        indicating if the request was valid and a String containing
+        the handshake response or None if the data was invalid.
         """
-        pass
+        resp = (False, None)
+        tokens = data.split("\r\n")
+
+        if ("Upgrade: websocket" not in tokens) or ("Connection: Upgrade" not in tokens):
+            return resp
+
+        for token in tokens[1:]:
+            label, value = token.split(": ", 1)
+            if label == "Sec-WebSocket-Key":
+                digest = WebSocketServer._digest(value)
+                resp = (True, WebSocketServer._HANDKSHAKE_RESP % (digest))
+                break
+
+        return resp
 
     @staticmethod
     def _digest(sec_key):
-        """Concatenates the sec_key with the _SEC_KEY and computes the SHA1 hash.
+        """Derives the Sec-Accept key from the client's Sec-Key.
 
-        :param sec_key: The sec_key provided in the upgrade request by a client.
+        :param sec_key: A String representing the Sec-Key provided in an upgrade request.
 
-        :returns: A base64 encoding of the SHA1 digest.
+        :returns: A utf-8 encoding of the Sec-Accept key.
         """
         raw = sec_key + WebSocketServer._SEC_KEY
-        return base64.b64encode(hashlib.sha1(raw.encode("ascii")).digest())
+        return base64.b64encode(hashlib.sha1(raw.encode("ascii")).digest()).decode("utf-8")
 
     def _initiate_close(self, client):
         """Sends the first Closing frame to the client.
