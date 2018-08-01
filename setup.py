b8f588f8d0a6d8244465655b83751cda811a7a20
Kai-Bailey <kai.bailey@icloud.com>
2018-07-31 22:42:43 -0600

    Fixed message box width issue.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 
 setup(name='websock',
-      version = '0.1.1',
+      version = '0.1.2',
       author = 'Kai Bailey',
       author_email = 'kbailey1@ualberta.ca',
       url = 'https://github.com/Kai-Bailey/websocket',
