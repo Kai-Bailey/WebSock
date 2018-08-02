cf8ea924f82f25a88daad1fe8bb076e454b30e45
Kai-Bailey <kai.bailey@icloud.com>
2018-08-01 21:04:58 -0600

    Fixed import error in unittests.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 
 setup(name='websock',
-      version = '0.1.4',
+      version = '0.1.5',
       author = 'Kai Bailey',
       author_email = 'kbailey1@ualberta.ca',
       url = 'https://github.com/Kai-Bailey/websocket',
--- tests/test_data_frame_decode.py
+++ tests/test_data_frame_decode.py
@@ -6,7 +6,7 @@ proj_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 socket_folder = os.path.join(proj_folder, 'websocket')
 sys.path.insert(0, socket_folder)
 
-import WebSocketServer as WS
+import websock as WS
 
 
 class TestDataFrameDecode(unittest.TestCase):
