1bd6ac9a4c055f090ebe2e38c4510116d5f43159
fbulbuc <fbulbuc@ualberta.ca>
2018-08-04 15:24:36 -0700

    Corrected version number.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 
 setup(name='websock',
-      version = '0.1.6',
+      version = '0.1.7',
       author = 'Kai Bailey',
       author_email = 'kbailey1@ualberta.ca',
       url = 'https://github.com/Kai-Bailey/websocket',
