3d11d47997780a025c11dfe8e018b28ecdb8aaa7
Kai-Bailey <kai.bailey@icloud.com>
2018-08-05 09:32:09 -0600

    Fixed title in chat app client.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 
 setup(name='websock',
-      version = '0.2.0',
+      version = '1.0.0',
       author = 'Kai Bailey',
       author_email = 'kbailey1@ualberta.ca',
       url = 'https://github.com/Kai-Bailey/websocket',
