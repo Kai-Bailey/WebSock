d88caa72e3b78f051650179b3ca32b2cb39bd26f
Haegi <benjamin@haegenlaeuer.de>
2019-10-30 09:39:43 +0100

    fixed import path

--- autobahn-test.py
+++ autobahn-test.py
@@ -1,5 +1,5 @@
 import threading
-from .WebSocketServer import WebSocketServer
+from websock.WebSocketServer import WebSocketServer
 
 server = WebSocketServer("127.0.0.1", 9010)
 server_thread = threading.Thread(target=server.serve_forever(), args=(), daemon=True)
