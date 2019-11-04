422bde1025ee0a168fb51ff86e45dfe1224b8767
Haegi <benjamin@haegenlaeuer.de>
2019-11-04 13:44:57 +0100

    adjusted push file for github usage

--- push.sh
+++ push.sh
@@ -3,16 +3,19 @@
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
+  echo -n > .gitignore
+  printf '# Ignore everything\n*\n# But not these files...\n!.gitignore\n# And include directories\n!reports/**/*' >> .gitignore
 }
 
 commit_website_files() {
   git status
-  git add .
+  git add reports
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin-pages https://2f513c1b0c8572971bdde13771667597f683f6d6@github.com/Haegi/WebSock.git > /dev/null 2>&1
+  git remote remove origin-pages
+  git remote add origin-pages https://0f1a98b5230491c575404356efa6286c29f9077c@github.com/Haegi/WebSock.git > /dev/null 2>&1
   git push --quiet --set-upstream origin-pages gh-pages 
 }
 
